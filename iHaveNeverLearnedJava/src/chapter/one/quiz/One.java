7a8a97994abead364c68a35c44e2c27c013b775d
kim seunghyun <kimseunghyun.bg@gmail.com>
2017-07-08 18:58:17 +0900

    Chapter1. done
    about comment
    
    Signed-off-by: kim seunghyun <kimseunghyun.bg@gmail.com>

---
+++ iHaveNeverLearnedJava/src/chapter/one/quiz/One.java
@@ -0,0 +1,17 @@
+package chapter.one.quiz;
+
+/**
+ * Created by kimseunghyun on 2017-07-08.
+ */
+public class One {
+    public static void main(String[] args) {
+        System.out.println("2+5="+2+5);
+        System.out.println("2+5="+(2+5));
+
+        System.out.println(12);
+        System.out.println("12");
+        System.out.println("1"+2);
+        System.out.println("1"+"2");
+        System.out.println(1+"2");
+    }
+}
---
+++ iHaveNeverLearnedJava/src/chapter/one/quiz/Two.java
@@ -0,0 +1,22 @@
+package chapter.one.quiz;
+
+/**
+ * Created by kimseunghyun on 2017-07-08.
+ */
+public class Two {
+    public static void main(String[] args) {
+        System.out.println("One" /* One은 1 */);
+        System.out.println("Two /* Two는 2 */ ");
+        System.out.println("Three");
+
+        /*
+        System.out.println(2);
+        //System.out.println("2");
+         */
+
+        /*
+        System.out.println(3);
+        //System.out.println("3");
+        */
+    }
+}
