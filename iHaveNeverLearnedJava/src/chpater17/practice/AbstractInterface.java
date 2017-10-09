ca57d9809a5b446d5e8759d0691df778db3d0f11
kim seunghyun <kimseunghyun.bg@gmail.com>
2017-10-09 11:54:06 +0900

    finish chapter 17
    Signed-off-by: kim seunghyun <kimseunghyun.bg@gmail.com>

---
+++ iHaveNeverLearnedJava/src/chpater17/practice/AbstractInterface.java
@@ -0,0 +1,66 @@
+package chpater17.practice;
+
+/**
+ * Created by kimseunghyun on 2017-10-08.
+ */
+
+abstract class PersonalNumberStorage {
+    public abstract void addPersonalInfo(String name, String perNum);
+
+    public abstract String searchName(String perNum);
+}
+
+class PersonalNumInfo {
+    private String name;
+    private String number;
+
+    public PersonalNumInfo(String name, String number) {
+        this.name = name;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
+
+class PersonalNumberStorageImpl extends PersonalNumberStorage {
+
+    private PersonalNumInfo[] perArr;
+    private int numOfPerInfo;
+
+    public PersonalNumberStorageImpl(int sz) {
+        perArr = new PersonalNumInfo[sz];
+        this.numOfPerInfo = 0;
+    }
+
+    @Override
+    public void addPersonalInfo(String name, String perNum) {
+        perArr[numOfPerInfo++] = new PersonalNumInfo(name, perNum);
+    }
+
+    @Override
+    public String searchName(String perNum) {
+        for (int i = 0; i < numOfPerInfo; i++) {
+            if (perNum.compareTo(perArr[i].getNumber()) == 0) {
+                return perArr[i].getName();
+            }
+        }
+        return null;
+    }
+}
+
+public class AbstractInterface {
+    public static void main(String[] args) {
+        PersonalNumberStorage storage = new PersonalNumberStorageImpl(100);
+        storage.addPersonalInfo("김기동", "950000-1122333");
+        storage.addPersonalInfo("장산길", "970000-1122334");
+
+        System.out.println(storage.searchName("950000-1122333"));
+        System.out.println(storage.searchName("970000-1122334"));
+    }
+}
---
+++ iHaveNeverLearnedJava/src/chpater17/quiz/MultiInheriAlternative.java
@@ -0,0 +1,58 @@
+package chpater17.quiz;
+
+/**
+ * Created by kimseunghyun on 2017-10-09.
+ */
+
+interface TV {
+    void onTV();
+}
+
+class TVImpl implements TV {
+    @Override
+    public void onTV() {
+        System.out.println("영상 출력 중");
+    }
+}
+
+interface Computer {
+    void dataRecieve();
+}
+
+class ComputerImpl implements Computer {
+    @Override
+    public void dataRecieve() {
+        System.out.println("영상 데이터 수신 중");
+    }
+}
+
+class IPTV implements Computer, TV {
+
+    ComputerImpl comp = new ComputerImpl();
+    TVImpl tv = new TVImpl();
+
+    @Override
+    public void dataRecieve() {
+        comp.dataRecieve();
+    }
+
+    @Override
+    public void onTV() {
+        tv.onTV();
+    }
+
+    public void powerOn() {
+        dataRecieve();
+        onTV();
+    }
+}
+
+public class MultiInheriAlternative {
+    public static void main(String[] args) {
+        IPTV iptv = new IPTV();
+        iptv.powerOn();
+
+        TV tv = iptv;
+        Computer comp = iptv;
+    }
+}
