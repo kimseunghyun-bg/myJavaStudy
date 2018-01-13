194deacbea9f0e85f46346bf089dcf051aa6a0a5
Seunghyun Kim <kimseunghyun.bg@gmail.com>
2018-01-13 16:33:43 +0900

    chapter22 ing...
    Signed-off-by: Seunghyun Kim <kimseunghyun.bg@gmail.com>

---
+++ iHaveNeverLearnedJava/src/chapter22/quiz/ArrayListBufSize.java
@@ -0,0 +1,17 @@
+package chapter22.quiz;
+
+import java.util.ArrayList;
+
+/**
+ * Created by kimseunghyun on 2018-01-13
+ */
+public class ArrayListBufSize {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.ensureCapacity(500);
+
+        list.add(0);
+        list.add(1);
+        list.ensureCapacity(list.size()*2);
+    }
+}
